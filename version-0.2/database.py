23396d195041326213bebf85cc7a1f27d510ef5e
miguelangelcollazos-code <miguel.angel.collazos@correounivalle.edu.co>
2025-11-28 16:05:57 -0500

    Update database.py

--- version-0.2/database.py
+++ version-0.2/database.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",    # <-- Pon tu contraseña si tienes
+        password="",
         database="petro"
     )
